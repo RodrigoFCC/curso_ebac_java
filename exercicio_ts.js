bb2c18586e9a13ba2fb516688a72a73a673ffe0c
Rodrigo Carvalho <rodrigofcc.dev@gmail.com>
2023-07-22 21:18:23 -0300

    exercicio ts

---
+++ exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+console.log(multiply(5, 7));
+function salutation(name) {
+    return `Olá ${name}`;
+}
+console.log(salutation("Pedro"));
---
+++ exercicio_ts.ts
@@ -0,0 +1,11 @@
+function multiply(number1: number, number2: number): number {
+    return number1 * number2;
+}
+
+console.log(multiply(5,7));
+
+function salutation(name: string): string {
+    return `Olá ${name}`;
+}
+
+console.log(salutation("Pedro"));
